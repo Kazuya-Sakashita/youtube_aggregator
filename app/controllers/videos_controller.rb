16f6674a8fdb2cb109a802d6bbfcf5fc8d974f7f
Kazuya-Sakashita <kz0508@gmail.com>
2024-12-23 12:27:06 +0900

    ボタン位置やデザイン変更

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -3,45 +3,64 @@ require 'google/apis/youtube_v3'
 class VideosController < ApplicationController
   before_action :set_youtube_service
 
+  # 動画検索アクション
   def search
-    if params[:query].present?
-      # YouTube APIのリクエストに並び替えオプション(order: 'viewCount')を追加
-      results = @youtube_service.list_searches('snippet', q: params[:query], max_results: 10, order: 'viewCount')
-      @videos = results.items.map do |item|
-        video_id = item.id.video_id
-        next unless video_id
+    query = params[:query]
+    category_id = params[:category]
 
-        video_details = @youtube_service.list_videos('statistics', id: video_id).items.first
-        next unless video_details
+    if query.present?
+      # YouTube APIの検索パラメータを設定
+      search_params = {
+        q: query,
+        max_results: 12,
+        type: 'video'
+      }
 
-        {
-          video_id: video_id,
-          title: item.snippet.title,
-          description: item.snippet.description,
-          thumbnail: item.snippet.thumbnails.high.url,
-          view_count: video_details.statistics.view_count
-        }
-      end.compact
+      # カテゴリIDが指定されている場合は検索パラメータに追加
+      search_params[:video_category_id] = category_id if category_id.present?
+
+      begin
+        # YouTube APIで動画を検索
+        results = @youtube_service.list_searches('snippet', **search_params)
+
+        # 検索結果を整形
+        @videos = results.items.map do |item|
+          video_id = item.id.video_id
+          next unless video_id
+
+          # 動画の統計情報を取得
+          video_details = @youtube_service.list_videos('statistics', id: video_id).items.first
+          next unless video_details
+
+          {
+            video_id: video_id,
+            title: item.snippet.title,
+            channel_title: item.snippet.channel_title,
+            view_count: video_details.statistics.view_count,
+            description: item.snippet.description,
+            thumbnail: item.snippet.thumbnails.high.url,
+            video_url: "https://www.youtube.com/watch?v=#{video_id}"
+          }
+        end.compact
+      rescue Google::Apis::ClientError => e
+        # エラー処理
+        Rails.logger.error("YouTube API error: #{e.message}")
+        flash[:alert] = "YouTube API error: #{e.message}"
+        @videos = []
+      end
     else
       @videos = []
     end
-  rescue Google::Apis::ClientError => e
-    Rails.logger.error("YouTube API error: #{e.message}")
-    flash[:alert] = "YouTube API error: #{e.message}"
-    @videos = []
   end
 
-
-
+  # 動画保存アクション
   def create
-    # 必要なパラメータを許可
+    # パラメータの許可
     video_data = params.require(:video).permit(:video_id, :title, :view_count, :description, :thumbnail)
 
     # 動画情報を保存または更新
     video = Video.find_or_initialize_by(video_id: video_data[:video_id])
-    video.update(video_data)
-
-    if video.persisted?
+    if video.update(video_data)
       flash[:notice] = "Video saved successfully!"
       redirect_to videos_path
     else
@@ -50,13 +69,15 @@ class VideosController < ApplicationController
     end
   end
 
-
+  # 動画一覧表示アクション
   def index
+    # 再生回数順に動画を表示
     @videos = Video.order(view_count: :desc).limit(10)
   end
 
   private
 
+  # YouTube APIのサービス設定
   def set_youtube_service
     @youtube_service ||= Google::Apis::YoutubeV3::YouTubeService.new.tap do |service|
       service.key = ENV['YOUTUBE_API_KEY']
